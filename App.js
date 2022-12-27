4695fff6c956c92ca68bb33168010bcd64242383
unknown <Savioanjos2020@gmail.com>
2022-12-27 08:31:14 -0300

    Configurando origem

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [nome, setNome] = useState('Fulano')
+
+  function handleMudaNome() {
+    if(nome === 'Fulano') {
+      setNome('Sujeito Programador')
+    } else {
+      setNome('Fulano')
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+    paddingTop: 28,
+  },
+
+
+})
